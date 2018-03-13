6bdd48985a10c47aad2f3f67091439215ee0a072
Student <student@MB-2.local>
2018-03-13 12:36:27 -0500

    Release 1.0! Settings, at a glance wpm colors, and more

--- 10ff.user.js
+++ 10ff.user.js
@@ -1,6 +1,9 @@
 // ==UserScript==
 // @name         SuperFastFingers
-// @version      0.1
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @version      1.0
 // @description  Show live wpm
 // @author       AFriendlyTrashcan (@7v1)
 // @match        https://10fastfingers.com/typing-test/*
@@ -9,12 +12,82 @@
 
 (function() {
     'use strict';
+    GM_config.init(
+        {
+            'id': 'SFF-conf',
+            'fields':
+            {
+                'aagColors':
+                {
+                    'label': 'Enable live WPM colors',
+                    'type': 'select',
+                    'options': ['true','false'],
+                    'default': 'True'
+                },
+                'greatWPM':
+                {
+                    'label': 'Choose what WPM triggers great background color',
+                    'type': 'text',
+                    'default': '150'
+                },
+                'goodWPM':
+                {
+                    'label': 'Choose what WPM triggers good background color',
+                    'type': 'text',
+                    'default': '140'
+                },
+                'normalWPM':
+                {
+                    'label': 'Choose what WPM triggers normal background color',
+                    'type': 'text',
+                    'default': '130'
+                },
+                'taran':
+                {
+                    'label': 'Enable WPM >1000',
+                    'type': 'select',
+                    'options': ['true','false'],
+                    'default': 'false'
+                },
+                'm00p':
+                {
+                    'label': 'Enable negative WPM',
+                    'type': 'select',
+                    'options': ['true','false'],
+                    'default': 'false'
+                }
+            }
+    });
+
+    var aagColors = true;
+    var greatWPM = 150;
+    var goodWPM = 140;
+    var normalWPM = 130;
     var taran = false;
+    var m00p = false;
     var realWPM;
+
+    $("#user-online-box").html("<a id='settingsSFF' href='javascript:void()'>SuperFastFingers settings</a>");
+    $("#settingsSFF").click(function(){
+        GM_config.open();
+    });
+
+    $('#preview').html("<font size='+3'><b>WPM:</b> " +
+        realWPM + "<br><b>Key Strokes:</b> " +
+        error_keystrokes + "<br><b>Words:</b> " +
+        error_correct + "</<font size='+3'>");
+
     $(document).keydown(function (k)
     {
-        realWPM = Math.round((error_keystrokes / 5) / ((60.01 - countdown) / 60));
-        if((realWPM > 1000 || taran) || realWPM < 0){
+        aagColors = (GM_config.get('aagColors') == 'true');
+        greatWPM = GM_config.get('greatWPM');
+        goodWPM = GM_config.get('goodWPM');
+        normalWPM = GM_config.get('normalWPM');
+        taran = (GM_config.get('taran') == 'true');
+        m00p = (GM_config.get('m00p') == 'true');
+
+        realWPM = Math.round((error_keystrokes / 4.98) / ((60.01 - countdown) / 60));
+        if((realWPM > 1000 && !taran) || realWPM < 0 && !m00p){
             realWPM = 0;
         }
 
@@ -24,5 +97,19 @@
         error_correct + "</<font size='+3'>");
 
         $('#words').before("<div id='preview'></div>");
+        if(aagColors){
+            if(realWPM > greatWPM){
+                $("#preview").css({"backgroundColor":"green","color":"black"});
+            }else if(realWPM > goodWPM){
+                $("#preview").css({"backgroundColor":"blue","color":"black"});
+            }else if(realWPM > normalWPM){
+                $("#preview").css({"backgroundColor":"orange","color":"black"});
+            }else if(realWPM == 0){
+                $("#preview").css({"backgroundColor":"white","color":"black"});
+            }else{
+                $("#preview").css({"backgroundColor":"red","color":"black"});
+            }
+        }
     });
 })();
+
