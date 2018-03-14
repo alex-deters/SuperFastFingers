8b74b6c47b9722b5ab461b8377005112cb66ed05
AFriendlyTrashcan <remade2.0@gmail.com>
2018-03-13 21:38:58 -0500

    Version 1.1, competition support

--- 10ff.user.js
+++ 10ff.user.js
@@ -2,11 +2,10 @@
 // @name         SuperFastFingers
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        GM_getValue
-// @grant        GM_setValue
-// @version      1.0
+// @version      1.1
 // @description  Show live wpm
 // @author       AFriendlyTrashcan (@7v1)
-// @match        https://10fastfingers.com/typing-test/*
+// @match        https://10fastfingers.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -22,7 +21,7 @@
                     'label': 'Enable live WPM colors',
                     'type': 'select',
                     'options': ['true','false'],
-                    'default': 'True'
+                    'default': 'true'
                 },
                 'greatWPM':
                 {
@@ -65,6 +64,9 @@
     var normalWPM = 130;
     var taran = false;
     var m00p = false;
+    var keystrokes;
+    var wordIndex = 0;
+    var correctWords;
     var realWPM;
 
     $("#user-online-box").html("<a id='settingsSFF' href='javascript:void()'>SuperFastFingers settings</a>");
@@ -72,44 +74,64 @@
         GM_config.open();
     });
 
-    $('#preview').html("<font size='+3'><b>WPM:</b> " +
-        realWPM + "<br><b>Key Strokes:</b> " +
-        error_keystrokes + "<br><b>Words:</b> " +
-        error_correct + "</<font size='+3'>");
+    $(document).keydown(function (k){
+        if(k.which == 32){
+            aagColors = (GM_config.get('aagColors') == 'true');
+            greatWPM = GM_config.get('greatWPM');
+            goodWPM = GM_config.get('goodWPM');
+            normalWPM = GM_config.get('normalWPM');
+            taran = (GM_config.get('taran') == 'true');
+            m00p = (GM_config.get('m00p') == 'true');
+            if(window.location.href.indexOf("competition") > -1) {
+                if(user_input_stream.split(" ").length == 2){
+                    row_counter--;
+                    previous_position_top = p.top;
+                    var zeilensprung_hoehe = (-1 * line_height) * row_counter;
+                    $("#row1").css('top', zeilensprung_hoehe+"px"); //bei einem zeilensprung wird der text um "line_height" verschoben
+                    $('#row1 span[wordnr="'+word_pointer+'"]').addClass('highlight');
+                    fill_line_switcher();
 
-    $(document).keydown(function (k)
-    {
-        aagColors = (GM_config.get('aagColors') == 'true');
-        greatWPM = GM_config.get('greatWPM');
-        goodWPM = GM_config.get('goodWPM');
-        normalWPM = GM_config.get('normalWPM');
-        taran = (GM_config.get('taran') == 'true');
-        m00p = (GM_config.get('m00p') == 'true');
-
-        realWPM = Math.round((error_keystrokes / 4.98) / ((60.01 - countdown) / 60));
-        if((realWPM > 1000 && !taran) || realWPM < 0 && !m00p){
-            realWPM = 0;
-        }
-
-        $('#preview').html("<font size='+3'><b>WPM:</b> " +
-        realWPM + "<br><b>Key Strokes:</b> " +
-        error_keystrokes + "<br><b>Words:</b> " +
-        error_correct + "</<font size='+3'>");
-
-        $('#words').before("<div id='preview'></div>");
-        if(aagColors){
-            if(realWPM > greatWPM){
-                $("#preview").css({"backgroundColor":"green","color":"black"});
-            }else if(realWPM > goodWPM){
-                $("#preview").css({"backgroundColor":"blue","color":"black"});
-            }else if(realWPM > normalWPM){
-                $("#preview").css({"backgroundColor":"orange","color":"black"});
-            }else if(realWPM == 0){
-                $("#preview").css({"backgroundColor":"white","color":"black"});
+                    keystrokes = 0;
+                    correctWords = 0;
+                    wordIndex = 0;
+                }
+                if(user_input_stream.split(" ")[wordIndex] == words[wordIndex]){
+                    keystrokes += words[wordIndex].length;
+                    correctWords++;
+                }else{
+                    keystrokes -= words[wordIndex].length;
+                }
+                wordIndex++;
             }else{
-                $("#preview").css({"backgroundColor":"red","color":"black"});
+                keystrokes = error_keystrokes;
+                correctWords = error_correct;
+            }
+            realWPM = Math.round((keystrokes / 5) / ((60.01 - countdown) / 60));
+            if((realWPM > 1000 && !taran) || realWPM < 0 && !m00p){
+                realWPM = 0;
+            }
+
+            $('#words').before("<div id='preview'></div>");
+            $('#preview').html("<font size='+3'><b>WPM:</b> " +
+                               realWPM + "<br><b>Key Strokes:</b> " +
+                               keystrokes + "<br><b>Words:</b> " +
+                               correctWords + "</<font size='+3'>");
+
+            if(aagColors){
+                if(realWPM > greatWPM){
+                    $("#preview").css({"backgroundColor":"green","color":"black"});
+                }else if(realWPM > goodWPM){
+                    $("#preview").css({"backgroundColor":"blue","color":"black"});
+                }else if(realWPM > normalWPM){
+                    $("#preview").css({"backgroundColor":"orange","color":"black"});
+                }else if(realWPM == 0){
+                    $("#preview").css({"backgroundColor":"white","color":"black"});
+                }else{
+                    $("#preview").css({"backgroundColor":"red","color":"black"});
+                }
             }
         }
     });
 })();
+
 
